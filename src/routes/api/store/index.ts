06b0939dd97fe920800afde2c0daf6765d181eca
Donato <donatotuzzolino@gmail.com>
2023-09-05 17:21:03 +0200

    update

--- src/routes/api/store/index.ts
+++ src/routes/api/store/index.ts
@@ -1,30 +1,28 @@
 import { type RequestHandler } from "@builder.io/qwik-city";
-/* import fs from "fs"; */
-import db from "../../../../db.json";
-/* import type { Product } from "~/types"; */
+import { readFileSync } from "fs";
+import type { Product } from "~/types";
 
 export const onGet: RequestHandler = async ({ json, query }) => {
   try {
-    /* const data = fs.readFileSync("db.json"); */
+    const data = readFileSync("db.json");
     const category = query.get("category") || "allCategory";
     const productName = query.get("productName")?.toLowerCase();
-    console.log("db", db);
-    /* const store = JSON.parse(data.toString()); */
-    /*    const productsData: Product[] = ; */
+    const store = JSON.parse(data.toString());
+    const productsData: Product[] = store.products;
     if (category !== "allCategory" && !productName) {
-      const data = db.products.filter((productName) =>
+      const data = productsData.filter((productName) =>
         productName.category.toLowerCase().includes(category)
       );
       json(200, { response: { products: data } });
     } else if (category === "allCategory" && productName) {
-      const data = db.products.filter(
+      const data = productsData.filter(
         (product) =>
           product.title.toLowerCase().includes(productName) ||
           product.description.toLowerCase().includes(productName)
       );
       json(200, { response: { products: data } });
     } else if (category !== "allCategory" && productName) {
-      const data = db.products.filter(
+      const data = productsData.filter(
         (product) =>
           product.category.toLowerCase().includes(category) &&
           (product.description.toLowerCase().includes(productName) ||
@@ -32,7 +30,7 @@ export const onGet: RequestHandler = async ({ json, query }) => {
       );
       json(200, { response: { products: data } });
     } else {
-      json(200, { response: db /*  JSON.parse(data.toString()) */ });
+      json(200, { response: JSON.parse(data.toString()) });
     }
   } catch (err) {
     json(500, { response: [] });
